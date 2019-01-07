47916d5fc7f04c03241c9a446d7c5562b102c1c0
Marc Reichelt <marc.reichelt@iteratec.de>
2019-01-07 08:42:43 +0100

    Migrate to Gradle Kotlin DSL
    - improves auto-completion and type safety
    - also create new buildSrc folder for library versions + dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.springframework.boot:spring-boot-gradle-plugin:${Versions.springBoot}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+        classpath("org.jetbrains.kotlin:kotlin-allopen:${Versions.kotlin}")
+    }
+}
+
+plugins {
+    java
+    kotlin("jvm") version Versions.kotlin
+    id("org.jetbrains.kotlin.plugin.spring") version Versions.kotlin
+    id("org.springframework.boot") version Versions.springBoot
+    id("io.spring.dependency-management") version "1.0.6.RELEASE"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compile("org.springframework.boot:spring-boot-starter-web:${Versions.springBoot}") {
+        exclude(module = "spring-boot-starter-tomcat")
+    }
+    compile("org.springframework.boot:spring-boot-starter-jetty:${Versions.springBoot}")
+    compile("org.springframework.boot:spring-boot-starter-data-jpa:${Versions.springBoot}")
+    compile("org.springframework.boot:spring-boot-starter-security:${Versions.springBoot}")
+    compile("org.springframework.boot:spring-boot-starter-actuator:${Versions.springBoot}")
+    compile("org.springframework.boot:spring-boot-devtools:${Versions.springBoot}")
+    compile("org.springframework:spring-tx")
+    compile("io.springfox:springfox-swagger2:${Versions.swagger}")
+    compile("io.springfox:springfox-swagger-ui:${Versions.swagger}")
+    compile("com.google.code.gson:gson:${Versions.gson}")
+    compile("org.postgresql:postgresql:42.2.5")
+    compile("com.microsoft.azure:azure-active-directory-spring-boot-starter:2.0.3")
+    compile("org.springframework.security:spring-security-oauth2-client:5.0.7.RELEASE")
+    compile("org.springframework.security:spring-security-oauth2-jose:5.0.7.RELEASE")
+    compile("org.flywaydb:flyway-core:${Versions.flyway}")
+    compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}")
+    compile("com.fasterxml.jackson.module:jackson-module-kotlin:2.9.8")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:${Versions.junit}")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Versions.junit}")
+    testCompile("org.springframework.boot:spring-boot-starter-test") {
+        exclude(module = "junit")
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.jar {
+    archiveBaseName.set("office-map")
+    archiveVersion.set("0.1.0")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,8 @@
+object Versions {
+    val kotlin = "1.3.11"
+    val springBoot = "2.0.3.RELEASE"
+    val swagger = "2.9.2"
+    val flyway = "5.2.1"
+    val gson = "2.8.0"
+    val junit = "5.2.0"
+}
